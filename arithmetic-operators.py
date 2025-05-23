30779f2ec23bd88521596de49acfd016587668fa
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-05-23 19:48:51 +0530

    Add files via upload

---
+++ arithmetic-operators.py
@@ -0,0 +1,13 @@
+a = 50
+b = 20
+
+print(a + b) # 70
+print(a - b) # 30
+print(a * b) # 1000
+print(a / b) # 2.5
+print(a // b) # 2
+print(a % b) # 10
+
+a = 3
+b = 2
+print(a ** 3) # 3 * 3 * 3 = 27
---
+++ assignment-operator.py
@@ -0,0 +1,14 @@
+a = 50
+print(a)
+
+a = 100
+print(a)
+
+a += 10  # a = a + 10
+print(a)
+
+a -= 10
+print(a)
+
+a *= 5
+print(a)
