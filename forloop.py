211e7707fd0eb30c10909ff65f764ce5eeee5654
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-06-12 20:11:46 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,24 @@
+for i in range(5):
+    print(i)
+print("______________")
+
+for i in range(0,20,2):
+    print(i)
+print("______________")
+
+for i in range(10,1):
+    print(i)
+print("______________")
+
+for i in range(1,10):
+    print(-i)
+print("______________")
+
+for i in range(-10,-1):
+    print(i)
+print("______________")
+
+studlist = ['alex','john','peter','henry','bob','alice']
+
+for s in studlist:
+    print(s)
---
+++ rangefunction.py
@@ -0,0 +1,9 @@
+print(range(1,100))
+
+
+range(startvalue,endvalue)
+startvalue - INCLUSIVE
+endvalue   - EXCLUSIVE
+
+range(value)
+range(20)
