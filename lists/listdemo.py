eee5f3825da28380672253e3f74be8ac88704e5e
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-06-17 20:05:03 +0530

    Add files via upload

---
+++ lists/listdemo.py
@@ -0,0 +1,17 @@
+mylist = [10,20,30,40,50]
+print(mylist)
+print(type(mylist))
+
+yourlist = list((100,200,300,400,500))
+print(yourlist)
+print(type(yourlist))
+
+mylist=[10, 20, 'Shravani', True, 9.5, 33.56, 1000]
+print(mylist)
+
+print(len(mylist))
+
+print(mylist[0])
+print(mylist[3])
+print(mylist[5])
+print(mylist[3-2])
