bdb4a1800c80a9c332b408f43da3fe5bb3862efa
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-07-11 20:33:27 +0530

    Add files via upload

---
+++ scopes/function-scope.py
@@ -0,0 +1,9 @@
+global yourfunction
+
+def myfunction():
+    print("THIS IS MY FUNCTION")
+    def yourfunction():
+        print("THIS IS YOUR FUNCTION")
+    
+    
+myfunction()
---
+++ scopes/scopes.py
@@ -0,0 +1,12 @@
+global x
+x = 10
+def showdata():
+    x = 40
+    y = 20
+    print(x + y)
+    
+
+showdata()
+
+print(x)
+# print(y)
